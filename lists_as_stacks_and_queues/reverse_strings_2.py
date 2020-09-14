c2668a7d19298d177089ceb884e49226a6df10ed
Ivaylo Savov <ivaylo_ss@abv.bg>
2020-09-14 18:23:35 +0300

    update reverse_strings_2.py

--- lists_as_stacks_and_queues/reverse_strings_2.py
+++ lists_as_stacks_and_queues/reverse_strings_2.py
@@ -1,5 +1,5 @@
 stack = list(input())
 
-while len(stack) > 0:
+while len(stack) > 0: # this is better solution than by for loop type cappaning mistake?
     item = stack.pop()
     print(item, end="")
