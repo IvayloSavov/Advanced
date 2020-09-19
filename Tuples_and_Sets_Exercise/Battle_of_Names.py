833ec02ee0cc681e4de25e85f2fd70d1586a86ca
Ivaylo Savov <ivaylo_ss@abv.bg>
2020-09-19 19:58:27 +0300

    Update project

--- Tuples_and_Sets_Exercise/Battle_of_Names.py
+++ Tuples_and_Sets_Exercise/Battle_of_Names.py
@@ -1,64 +1,37 @@
-num_lines = int(input())
+def even_odd_numbers(count_n):
 
-set_even = set()
-set_odd = set()
+    even_numbers = set()
+    odd_numbers = set()
 
-for i in range(num_lines):
-    sum_chars = sum(map(ord, input()))
-    div = sum_chars // (i + 1)
+    for i in range(1, count_n + 1):
+        name_sum = sum(tuple(map(lambda ch: ord(ch), input())))
+        v = name_sum // i
+        if v % 2 == 0:
+            even_numbers.add(v)
+        else:
+            odd_numbers.add(v)
 
-    if div % 2 == 0:
-        set_even.add(div)
-    else:
-        set_odd.add(div)
+    return even_numbers, odd_numbers
 
-sum_even = sum(set_even)
-sum_odd = sum(set_odd)
 
-list_even = list(map(str, set_even))
-list_odd = list(map(str, set_odd))
+def operation(even_number: set, odd_number: set):
 
-if sum_even == sum_odd:
-    list_output = list(map(str, set_even | set_odd))
-    print(", ".join(list_output))
-elif sum_odd > sum_even:
-    list_output = list(map(str, set_even - set_odd))
-    print(", ".join(list_output))
-else:
-    list_output = list(map(str, set_even ^ set_odd))
-    print(", ".join(list_output))
+    result = set()
 
-# Solving from colleague
-# number = int(input())
-# char_sum = 0
-#
-# even_nums = set()
-# odd_nums = set()
-#
-# for num in range(1, number + 1):
-#     name = input()
-#     char_sum = 0
-#     for char in name:
-#         char_sum += ord(char)
-#
-#     devised_num = char_sum // num
-#     if devised_num % 2 == 0:
-#         even_nums.add(devised_num)
-#     else:
-#         odd_nums.add(devised_num)
-#
-# even_nums_sum = sum(even_nums)
-# odd_nums_sum = sum(odd_nums)
-#
-# if even_nums_sum == odd_nums_sum:
-#     union_values = set(even_nums | odd_nums)
-#     union_values = [str(x) for x in union_values]
-#     print(f"{', '.join(union_values)}")
-# elif odd_nums_sum > even_nums_sum:
-#     different = set(odd_nums - even_nums)
-#     different = [str(x) for x in different]
-#     print(f"{', '.join(different)}")
-# elif even_nums_sum > odd_nums_sum:
-#     symmetric_different = set(even_nums ^ odd_nums)
-#     symmetric_different = [str(x) for x in symmetric_different]
-#     print(f"{', '.join(symmetric_different)}")
+    if sum(even_number) == sum(odd_number):
+        result = odd_number.union(even_number)
+    elif sum(odd_number) > sum(even_number):
+        result = odd_number.difference(even_number)
+    elif sum(even_number) > sum(odd_number):
+        result = odd_number.symmetric_difference(even_number)
+
+    return result
+
+
+def printing_the_result(res):
+    print(", ".join(tuple(map(str, res))))
+
+
+n = int(input())
+even_numbers, odd_numbers = even_odd_numbers(n)
+printing_the_result(operation(even_numbers, odd_numbers))
